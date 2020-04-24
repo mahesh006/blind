80df549ff956541d801af71c4f7352449bc3da23
mahesh006 <55144460+mahesh006@users.noreply.github.com>
2020-04-24 03:27:30 -0700

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://localhost:4000');
+var socket = io.connect('https://blind123.herokuapp.com/');
 
 var message = document.getElementById('message'),
       handle = document.getElementById('handle'),
