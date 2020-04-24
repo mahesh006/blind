afe10f9b1d53f49ca1317fec24fe98a51b3dcd6a
mahesh006 <55144460+mahesh006@users.noreply.github.com>
2020-04-24 05:05:34 -0700

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,4 @@
-var socket = io.connect('https:localhost:4000');
+var socket = io.connect('http:localhost:4000');
 
 var message = document.getElementById('message'),
       handle = document.getElementById('handle'),
